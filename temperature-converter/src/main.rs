79555dd27938ead7f60d106ae1f65d23d4a8159a
Jeferson Oliveira <zuruckyt@gmail.com>
2023-10-08 22:59:01 -0300

    feat: temperature converter inputs

--- temperature-converter/src/main.rs
+++ temperature-converter/src/main.rs
@@ -1,3 +1,36 @@
+use dialoguer::{Select, Input};
+
 fn main() {
-    println!("Hello, world!");
+    let items = vec!["Celsius ºC", "Farenheit ºF", "Kelvin K"];
+
+    let convert_from = Select::new()
+        .with_prompt("From which unit do you want to convert from?")
+        .items(&items)
+        .interact()
+        .unwrap_or_default();
+    
+    println!("You chose to convert from {}", items[convert_from]);
+
+    let filtered_items = items
+        .iter()
+        .enumerate()
+        .filter(|(usize, &&_)|  *usize != convert_from)
+        .map(|(_, &item)| item.to_owned())
+        .collect::<Vec<String>>();
+
+    let convert_into = Select::new()
+        .with_prompt("Which unit do you wish to convert into?")
+        .items(&filtered_items)
+        .interact()
+        .unwrap_or_default();
+
+    println!("You chose to convert into {}", items[convert_into]);
+
+    let original_temperature: f32 = Input::new()
+        .default(10.0)
+        .with_prompt("Enter the temperature:")
+        .interact()
+        .unwrap_or_default();
+
+    println!("Temperature in {}: {}", items[convert_from], original_temperature)
 }
